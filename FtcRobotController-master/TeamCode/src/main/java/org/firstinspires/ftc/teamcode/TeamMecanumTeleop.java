36103f8ed5fb60fc8b46b963fff49e3bf8e294e8
Island puter supreme <eklabel4321@gmail.com>
2024-10-12 13:07:29 -0500

    MecanumTeleop addition

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMecanumTeleop.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "TeamMecanumTeleop")
+public class TeamMecanumTeleop extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //initizlaizaiton
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        DcMotor backLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
+        double drive = -gamepad1.left_stick_y;
+        double turn = gamepad1.left_stick_x;
+        double strafe = gamepad1.right_stick_x;
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        waitForStart();
+
+        while(opModeIsActive()){
+
+        frontRight.setPower(drive-turn-strafe);
+        backRight.setPower(drive-turn+strafe);
+        frontLeft.setPower(drive+turn+strafe);
+        backLeft.setPower(drive+turn-strafe);
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
