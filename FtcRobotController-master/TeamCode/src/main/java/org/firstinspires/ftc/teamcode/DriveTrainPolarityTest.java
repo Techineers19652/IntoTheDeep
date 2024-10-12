9946fe0bbcd805b79d7aaae7dcdcd6122e9b4857
Island puter supreme <eklabel4321@gmail.com>
2024-10-12 15:41:11 -0500

    testing polarity code (also basic drive)

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainPolarityTest.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "DriveTrainPolarityTest")
+public class DriveTrainPolarityTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        DcMotor backLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+
+            ///if everythign is set right it should go forward
+
+            if(gamepad1.y) {
+                frontLeft.setPower(1);
+                backLeft.setPower(1);
+                frontRight.setPower(1);
+                backRight.setPower(1);
+            }
+            if(gamepad1.a) {
+                frontLeft.setPower(-1);
+                backLeft.setPower(-1);
+                frontRight.setPower(-1);
+                backRight.setPower(-1);
+            }
+            if(gamepad1.b){
+                frontLeft.setPower(1);
+                backLeft.setPower(1);
+                frontRight.setPower(-1);
+                backRight.setPower(-1);
+
+            }
+                if(gamepad1.x){
+                    frontLeft.setPower(-1);
+                    backLeft.setPower(-1);
+                    frontRight.setPower(1);
+                    backRight.setPower(1);
+                }
+
+
+        }
+
+    }
+}
